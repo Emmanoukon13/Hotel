e6f111a3862970ed17067da1bc27a43746793510
Emmanoukon13 <142174332+Emmanoukon13@users.noreply.github.com>
2023-09-15 15:54:44 +0300

    Add files via upload

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+
+  registerForm=this.fb.group({
+    username:['',Validators.required],
+    email:['',[Validators.required,Validators.email]],
+    password1:['',Validators.required],
+    password2:['',Validators.required],
+  });
+
+
+  IsSubmitted=false;
+
+  constructor(private fb:FormBuilder){}
+
+  
+
+  onSubmit():void{
+
+    console.log('submitted from',
+     this.registerForm.value,
+     this.registerForm.valid,
+     this.registerForm.get('password1')?.value ==this.registerForm.get('password2')?.value,
+     );
+     this.IsSubmitted=true;
+  }
 
 }
